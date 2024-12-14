3c06c62f5ffe768ab8bfcdcd8197fcb751630b79
kurrrru <kurrrru8850@gmail.com>
2024-12-14 19:50:45 +0900

    resolve data race

--- philo/src/supervisor.c
+++ philo/src/supervisor.c
@@ -6,13 +6,15 @@
 /*   By: nkawaguc <nkawaguc@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/12 15:13:21 by nkawaguc          #+#    #+#             */
-/*   Updated: 2024/12/13 11:53:11 by nkawaguc         ###   ########.fr       */
+/*   Updated: 2024/12/14 19:50:29 by nkawaguc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/philo.h"
 
 static void	die_to_starvation(t_philo *philo, int i);
+static long	get_last_meal(t_philo *philo, int i);
+static int	get_meal_cnt(t_philo *philo, int i);
 
 void	*supervisor(void *p)
 {
@@ -27,12 +29,12 @@ void	*supervisor(void *p)
 		i = -1;
 		while (++i < philo->config.num_philo)
 		{
-			if (philo->persons[i].last_meal && philo->config.time_to_die
-				< getms() - philo->persons[i].last_meal)
+			if (get_last_meal(philo, i) && philo->config.time_to_die
+				< getms() - get_last_meal(philo, i))
 				return (die_to_starvation(philo, i), NULL);
-			if (philo->persons[i].last_meal
-				&& philo->persons[i].meal_cnt < min_meal_cnt)
-				min_meal_cnt = philo->persons[i].meal_cnt;
+			if (get_last_meal(philo, i)
+				&& get_meal_cnt(philo, i) < min_meal_cnt)
+				min_meal_cnt = get_meal_cnt(philo, i);
 		}
 		pthread_mutex_lock(&philo->meal_mutex);
 		philo->min_meal_cnt = min_meal_cnt;
@@ -48,4 +50,24 @@ static void	die_to_starvation(t_philo *philo, int i)
 	pthread_mutex_lock(&philo->end_mutex);
 	philo->end = true;
 	pthread_mutex_unlock(&philo->end_mutex);
+}
+
+static long	get_last_meal(t_philo *philo, int i)
+{
+	long	last_meal;
+
+	pthread_mutex_lock(philo->persons[i].meal_mutex);
+	last_meal = philo->persons[i].last_meal;
+	pthread_mutex_unlock(philo->persons[i].meal_mutex);
+	return (last_meal);
+}
+
+static int	get_meal_cnt(t_philo *philo, int i)
+{
+	int	meal_cnt;
+
+	pthread_mutex_lock(philo->persons[i].meal_mutex);
+	meal_cnt = philo->persons[i].meal_cnt;
+	pthread_mutex_unlock(philo->persons[i].meal_mutex);
+	return (meal_cnt);
 }
